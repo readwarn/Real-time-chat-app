f6de4d362b51c6e134c3d892bce9f03d789395ab
Rilwan <akinyusuf5@gmail.com>
2023-04-09 21:50:13 +0100

    updated express session cookie option

--- server/config/oauthStrategy.js
+++ server/config/oauthStrategy.js
@@ -1,59 +1,76 @@
-const passport=require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GitHubStrategy = require('passport-github').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
-const localStrategy=require('passport-local').Strategy;
-const UserServices=require('../models/user');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const GitHubStrategy = require("passport-github").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const localStrategy = require("passport-local").Strategy;
+const UserServices = require("../models/user");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "766284901009-addbg5mgubtrnv5doie939saihjsr530.apps.googleusercontent.com",
+      clientSecret: "LhNiJw8ZqbyWw64-Fzkdyt1i",
+      callbackURL:
+        "https://whispering-everglades-42925.herokuapp.com/auth/google/redirect",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      UserServices.findOrCreate(UserServices.User, done, profile);
+    }
+  )
+);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: "0ff9f174008c98f84fc6",
+      clientSecret: "3a059e3bd45cc324f11f0555de0a874bcfdb45e3",
+      callbackURL:
+        "https://whispering-everglades-42925.herokuapp.com/auth/github/redirect",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      UserServices.findOrCreate(UserServices.User, done, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "2599125657002783",
+      clientSecret: "bb2ff8ece7f15a10f5d1cedce0d20e20",
+      callbackURL:
+        "https://whispering-everglades-42925.herokuapp.com/auth/facebook/redirect",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      UserServices.findOrCreate(UserServices.User, done, profile);
+    }
+  )
+);
+
+passport.use(
+  new localStrategy((username, password, done) => {
+    UserServices.User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  })
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  UserServices.User.findById(id,function(err,user){
-      done(err,user)
+  UserServices.User.findById(id, function (err, user) {
+    done(err, user);
   });
 });
-
-passport.use(new GoogleStrategy({
-    clientID: '766284901009-addbg5mgubtrnv5doie939saihjsr530.apps.googleusercontent.com',
-    clientSecret: 'LhNiJw8ZqbyWw64-Fzkdyt1i',
-    callbackURL: 'https://whispering-everglades-42925.herokuapp.com/auth/google/redirect'
-  },
-   function (accessToken, refreshToken, profile, done){
-    UserServices.findOrCreate(UserServices.User,done,profile);
-   }
-));
-
-passport.use(new GitHubStrategy({
-    clientID: '0ff9f174008c98f84fc6',
-    clientSecret: '3a059e3bd45cc324f11f0555de0a874bcfdb45e3',
-    callbackURL: 'https://whispering-everglades-42925.herokuapp.com/auth/github/redirect'
-  },
-  function(accessToken, refreshToken, profile, done) {
-    UserServices.findOrCreate(UserServices.User,done,profile);
-  }
-));
-
-passport.use(new FacebookStrategy({
-  clientID: '2599125657002783',
-  clientSecret: 'bb2ff8ece7f15a10f5d1cedce0d20e20',
-  callbackURL: "https://whispering-everglades-42925.herokuapp.com/auth/facebook/redirect"
-},
-function(accessToken, refreshToken, profile, done) {
-     UserServices.findOrCreate(UserServices.User,done,profile);
-}
-));
-
-passport.use(new localStrategy((username,password,done)=>{
-  UserServices.User.findOne({ username: username }, function(err, user) {
-    if (err) { return done(err); }
-    if (!user) {
-       return done(null, false, { message: 'Incorrect username.' });
-    }
-    if (user.password!==password) {
-       return done(null, false, { message: 'Incorrect password.' });
-    }
-    return done(null, user);
-  })
-}))
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,82 +1,105 @@
-const router=require('express').Router();
-const passport=require('passport');
-const UserServices = require('../models/user');
-const Channel=require('../models/channel');
-const config=require('../config/oauthStrategy');
+const router = require("express").Router();
+const passport = require("passport");
+const UserServices = require("../models/user");
+const Channel = require("../models/channel");
+const config = require("../config/oauthStrategy");
 
-router.get('/google',passport.authenticate('google',{scope:['profile']}));
-router.get('/google/redirect',passport.authenticate('google'),(req,res)=>{
-  res.redirect('https://calm-meadow-71961.herokuapp.com/');
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  res.redirect("https://real-time-chat-app-c18p.onrender.com/");
 });
 
-router.get('/github',passport.authenticate('github',{scope:['user:email']}));
+router.get(
+  "/github",
+  passport.authenticate("github", { scope: ["user:email"] })
+);
 
-router.get('/github/redirect',passport.authenticate('github'),(req,res)=>{
-    res.redirect('https://calm-meadow-71961.herokuapp.com/');
+router.get("/github/redirect", passport.authenticate("github"), (req, res) => {
+  res.redirect("https://real-time-chat-app-c18p.onrender.com/");
 });
 
-router.get('/facebook',passport.authenticate('facebook'));
+router.get("/facebook", passport.authenticate("facebook"));
 
-router.get('/facebook/redirect',passport.authenticate('facebook'),(req,res)=>{
-  res.redirect('https://calm-meadow-71961.herokuapp.com/');
-});
+router.get(
+  "/facebook/redirect",
+  passport.authenticate("facebook"),
+  (req, res) => {
+    res.redirect("https://real-time-chat-app-c18p.onrender.com/");
+  }
+);
 
-router.post('/register', function (req,res,next){
-     UserServices.User.findOne({username:req.body.username}).then((user)=>{
-         if(user){
-           return res.json({message:'Username is already taken',loggedIn:false});
-         }
-         else{
-           UserServices.User.create({
-                username:req.body.username,
-                password:req.body.password
-           },function(err,newUser){
-            Channel.findById('5fc52db79aa9fb091c81c332',function(err,channel){
-              if(err){
-                res.send('error finding welcome channel')
-              }else{
-                newUser.channels.push(channel);
-                newUser.save();
-                channel.members.push(newUser);
-                channel.save();
+router.post("/register", function (req, res, next) {
+  UserServices.User.findOne({ username: req.body.username }).then((user) => {
+    if (user) {
+      return res.json({
+        message: "Username is already taken",
+        loggedIn: false,
+      });
+    } else {
+      UserServices.User.create(
+        {
+          username: req.body.username,
+          password: req.body.password,
+        },
+        function (err, newUser) {
+          Channel.findById("5fc52db79aa9fb091c81c332", function (err, channel) {
+            if (err) {
+              res.send("error finding welcome channel");
+            } else {
+              newUser.channels.push(channel);
+              newUser.save();
+              channel.members.push(newUser);
+              channel.save();
+            }
+          });
+          passport.authenticate("local", function (err, user, info) {
+            if (err) {
+              return next(err);
+            }
+            if (!user) {
+              return res.json({ message: info.message, loggedIn: false });
+            }
+            req.login(user, function (err) {
+              if (err) {
+                return next(err);
               }
-            })
-            passport.authenticate('local', function(err, user, info) {
-              if (err) { return next(err) }
-              if (!user) {
-                return res.json({ message: info.message, loggedIn:false })
-              }
-              req.login(user, function(err) {
-                if (err) { return next(err); }
-                return res.json({loggedIn:true});
-              });
-            })(req, res, next);
-          })
-         }
-     });
-})
-
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err) }
-    if (!user) {
-      return res.json({ message: info.message, loggedIn:false })
+              return res.json({ loggedIn: true });
+            });
+          })(req, res, next);
+        }
+      );
     }
-    req.logIn(user, function(err) {
-      if (err) { return next(err); }
-      return res.json({loggedIn:req.isAuthenticated(), user:req.user,session:req.session});
+  });
+});
+
+router.post("/login", function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.json({ message: info.message, loggedIn: false });
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      return res.json({
+        loggedIn: req.isAuthenticated(),
+        user: req.user,
+        session: req.session,
+      });
     });
   })(req, res, next);
 });
 
-router.get('/status',(req,res)=>{
-    res.json(req.isAuthenticated());
-})
-
-router.get('/logout',(req,res)=>{
-   req.logout();
-   res.send(req.user);
+router.get("/status", (req, res) => {
+  res.json(req.isAuthenticated());
 });
 
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.send(req.user);
+});
 
-module.exports=router;
+module.exports = router;
